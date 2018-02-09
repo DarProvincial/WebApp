a6de8667ce7377fcfbb2ccc793140b7c71f9fc28
judeescol1997 <34189306+judeescol1997@users.noreply.github.com>
2018-02-09 12:40:12 +0800

    Add files via upload

--- section_admin/php/php_available.php
+++ section_admin/php/php_available.php
@@ -8,7 +8,7 @@ $sql = mysqli_query($conn,"SELECT * FROM tbl_file WHERE emp_id='0' AND dept_id='
 $files = "";
 $num = mysqli_num_rows($sql);
 while($row = mysqli_fetch_array($sql)){
-	$files.="<li class=\"list-group-item\" id=\"".$row["file_id"]."↕".$row["file_name"]."\"  draggable=\"true\" ondragstart=\"drag(event, this.id)\"><i class=\"fa fa-file\"></i> ".$row["file_name"]."<span class=\"pull-right\"><button id=\"".$row["file_id"]."\" onclick=\"removeFile(2, this.id)\" class=\"btn btn-danger btn-xs\"><i class=\"fa fa-trash-o\"></i> </button></span></li>";
+	$files.="<li class=\"list-group-item\" id=\"".$row["file_id"]."↕".$row["file_name"]."\"  draggable=\"true\" ondragstart=\"drag(event, this.id)\"><span class=\"pull-right\"><button id=\"".$row["file_id"]."\" onclick=\"removeFile(2, this.id)\" class=\"btn btn-danger btn-xs\"><i class=\"fa fa-trash-o\"></i> </button></span><div style=\"white-space: nowrap; width: 90%; overflow: hidden;text-overflow: ellipsis;\"><i class=\"fa fa-file\"></i> ".$row["file_name"]."</div></li>";
 }
 
 echo json_encode(array("files"=>$files,"num"=>$num));
